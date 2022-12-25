43905be7a7cc9de0b96c58328750a7d2b5c5591c
coconoca-8039 <coconoca.8039@gmail.com>
2022-12-25 20:51:55 +0900

    p.81

--- C++_Practice.cpp
+++ C++_Practice.cpp
@@ -9,41 +9,14 @@
 
 using namespace std;  //名前空間
 
-namespace A {
-    int power;
-}
-namespace B {
-    int power;
-}
-
-//関数のプロトタイプ宣言
-void funcA();
-void funcB();
+bool judge(int);
 
 int main(int argc, char** argv){
-
-    A::power = 20;  //名前空間Aへ代入
-    B::power = 30;  //名前空間Bへ代入
-    cout << "main" << endl;
-    cout << "main:A::power" << A::power << endl;
-    cout << "main:B::power" << B::power << endl;
-    funcA();
-    funcB();
+    int num;
+    cout << "整数の値を入力";
+    cin >> num;
+    if (num % 2 == 0){
+        cout << num << "は偶数です" << endl;
+    }
     return 0;
-
 }
-
-void funcA(){
-    using namespace A;
-    cout << "funcA" << endl;
-    cout << "funcA:A::power=" << power << endl;
-    cout << "funcA:B::power=" << B::power << endl;
-    power = 40;
-}
-
-void funcB(){
-    using namespace B;
-    cout << "funcB" << endl;
-    cout << "funcA:A::power=" << A::power << endl;
-    cout << "funcA:B::power=" << power << endl; 
-}       
